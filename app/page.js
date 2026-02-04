48b378bec0c66f8e7d2b81d21a202ed3f2ced01c
sportyskater69 <rowan.feland@edu.sait.ca>
2026-02-04 15:50:18 -0700

    created file structure for week-4

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">week-2</Link><br />
-      <Link href="/week-3">week-3</Link>
+      <Link href="/week-3">week-3</Link><br />
+      <Link href="/week-4">week-4</Link>
     </main>
   );
 }
