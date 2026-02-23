e83a5344ae093c5f9678fd0b1214b47606d543c8
sportyskater69 <rowan.feland@edu.sait.ca>
2026-02-23 12:15:08 -0700

    finished part 2, NewItem and item-list updated

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Link href="/week-3">week-3</Link><br />
       <Link href="/week-4">week-4</Link><br />
       <Link href="/week-5">week-5</Link><br />
+      <Link href="/week-6">week-6</Link><br />
     </main>
   );
 }
--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,17 +1,55 @@
-import Item from "./item.js";
-import item from "./items.json";
+'use client'
 
-export default function ItemList() {
+import { useState } from "react";
+import Item from "./item.js";
+
+
+export default function ItemList({ items }) {
+    const [sortBy, setSortBy] = useState('name');
+
+    const itemsSorted = [...items].sort((x, y) => {
+        if (sortBy === "name") {
+            return x.name.localeCompare(y.name);
+        } else if (sortBy === "category") {
+            return x.category.localeCompare(y.category);
+        }
+        return 0;
+    })
     return (
-        <ul>
-            {item.map((item) =>
-                <Item
-                    key={item.id}
-                    name={item.name}
-                    quantity={item.quantity}
-                    category={item.category}
-                />
-            )}
-        </ul>
+        <div>
+            <div className="">
+                Sort By:
+                <button
+                    onClick={() => setSortBy("name")}
+                    className={`px-4 py-2 rounded ml-1 mr-1 mb-1 ${sortBy === "name"
+                        ? "bg-blue-700 text-white"
+                        : "bg-gray-200 dark:bg-gray-700 dark:text-white"
+                        }`}
+                >
+                    Name
+                </button>
+
+                <button
+                    onClick={() => setSortBy("category")}
+                    className={`px-4 py-2 rounded ${sortBy === "category"
+                        ? "bg-blue-700 text-white"
+                        : "bg-gray-200 dark:bg-gray-700 dark:text-white"
+                        }`}
+                >
+                    Category
+                </button>
+            </div>
+
+            <ul>
+                {itemsSorted.map((item) => (
+                    <Item
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        category={item.category}
+                    />
+                ))}
+            </ul>
+        </div>
     );
 }
