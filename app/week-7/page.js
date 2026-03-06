1c2ba5c9c88a34cccbe22d2faf58647b3e3560c8
sportyskater69 <rowan.feland@edu.sait.ca>
2026-03-06 12:09:36 -0700

    just changed a few things in page

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -2,17 +2,17 @@
 import { useState } from "react";
 import ItemList from "./item-list";
 import NewItem from "./NewItem";
-import item from "./items.json"
+import itemsData from "./items.json"
 
 export default function Page() {
-    const [items, setItems] = useState(item);
+    const [items, setItems] = useState(itemsData);
 
     const handleAddItem = (newItem) => {
         setItems((lastItem) => [...lastItem, newItem]);
     };
 
     return (
-        <main className=" bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-8 transition-colors min-h-screen flex flex-col items-center ">
+        <main className=" bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-8 transition-colors min-h-screen flex flex-col items-center m-0 ">
             <h1 className="text-3xl font-bold text-center">
                 Shopping List
             </h1>
